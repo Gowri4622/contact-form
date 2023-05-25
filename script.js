48f73038652c66fd50d2f8c26bae5387f28080be
Gowri4622 <gowrihifi@gmail.com>
2023-05-25 21:43:28 +0530

    done

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const firebaseConfig = {
     //   copy your firebase config informations
     apiKey: "AIzaSyAGYX46GfzYsfWIiNtMIM10ni1KYVDoooI",
-  authDomain: "contact-form-3306f.firebaseapp.com",
-  databaseURL: "https://contact-form-3306f-default-rtdb.firebaseio.com",
-  projectId: "contact-form-3306f",
-  storageBucket: "contact-form-3306f.appspot.com",
-  messagingSenderId: "69194912137",
-  appId: "1:69194912137:web:3bbbdc79fc20688af11158",
-  measurementId: "G-W7J74DZCJM"
+    authDomain: "contact-form-3306f.firebaseapp.com",
+    databaseURL: "https://contact-form-3306f-default-rtdb.firebaseio.com",
+    projectId: "contact-form-3306f",
+    storageBucket: "contact-form-3306f.appspot.com",
+    messagingSenderId: "69194912137",
+    appId: "1:69194912137:web:3bbbdc79fc20688af11158",
+    measurementId: "G-W7J74DZCJM"
   };
   
   // initialize firebase
@@ -22,10 +22,24 @@ const firebaseConfig = {
     e.preventDefault();
   
     var name = getElementVal("name");
-    var emailid = getElementVal("emailid");
-    var msgContent = getElementVal("msgContent");
+    var email = getElementVal("email");
+    var age=getElementVal("age")
+    var phone=getElementVal("phone")
+    var gender=getElementVal("gender")
+    var year=getElementVal("year")
+    var branch=getElementVal("branch")
+    var section=getElementVal("section")
+    var vaccine=getElementVal("vaccine")
+    var dose1taken=getElementVal("dose1taken")
+    var dose1type=getElementVal("dose1type")
+    var dose1side=getElementVal("dose1sideeff")
+    var covidafdose1=getElementVal("covidafdose1")
+    var dose2taken=getElementVal("dose2taken")
+    var dose2side=getElementVal("dose2sideeff")
+    var covidafdose2=getElementVal("covidafdose2")
+    var booster = getElementVal("booster");
   
-    saveMessages(name, emailid, msgContent);
+    saveMessages(name, email, age, phone, gender, year, branch, section, vaccine, dose1taken, dose1type, dose1side, covidafdose1, dose2taken, dose2side, covidafdose2, booster);
   
     //   enable alert
     document.querySelector(".alert").style.display = "block";
@@ -39,16 +53,52 @@ const firebaseConfig = {
     document.getElementById("contactForm").reset();
   }
   
-  const saveMessages = (name, emailid, msgContent) => {
+  const saveMessages = (name, email, age, phone, gender, year, branch, section, vaccine, dose1taken, dose1type, dose1side, covidafdose1, dose2taken, dose2side, covidafdose2, booster) => {
     var newContactForm = contactFormDB.push();
   
     newContactForm.set({
       name: name,
-      emailid: emailid,
-      msgContent: msgContent,
+      email:email, 
+      age:age, 
+      phone:phone, 
+      gender:gender, 
+      year:year, 
+      branch:branch, 
+      section:section, 
+      vaccine:vaccine, 
+      dose1taken:dose1taken, 
+      dose1type:dose1type, 
+      dose1side:dose1side, 
+      covidafdose1:covidafdose1, 
+      dose2taken:dose2taken, 
+      dose2side:dose2side, 
+      covidafdose2:covidafdose2, 
+      booster:booster
     });
   };
   
   const getElementVal = (id) => {
     return document.getElementById(id).value;
   };
+
+  function enableBrand(answer){
+    console.log(answer.value);
+    if(answer.value=="Yes"){
+        document.getElementById('carbrand').classList.remove('form-hide');
+        
+    }
+    else{
+        document.getElementById('carbrand').classList.add('form-hide'); 
+    }
+};
+
+function enableBrand1(answer){
+  console.log(answer.value);
+  if(answer.value=="Yes"){
+      document.getElementById('carbrand1').classList.remove('form-hide1');
+      
+  }
+  else{
+      document.getElementById('carbrand1').classList.add('form-hide1'); 
+  }
+};
